8c708feb99a0bd489999f33e934a08984ab67409
Ronald Glover <ron881212@gmail.com>
2019-02-19 17:58:54 -0500

    handled all routes

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendArray = [
+    {
+      name: "Puppet",
+      photo: "http://www.leaderpost.com/mobile/news/latest-news/cms/binary/5762351.jpg?size=mobile290",
+      scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
+  ];
+  
+module.exports = friendArray;
+
+
+
+  
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+ 
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
