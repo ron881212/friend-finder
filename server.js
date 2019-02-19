ecbac5bbb376ab8deeb22ebcfb0eaf47503fca89
Ronald Glover <ron881212@gmail.com>
2019-02-19 15:21:07 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
