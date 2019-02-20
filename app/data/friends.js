bc7d1f4e02ad6ff229bc2a56770cfb90ea5c9f7f
Ronald Glover <ron881212@gmail.com>
2019-02-20 05:02:42 -0500

    added logic and friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,22 @@
-var friendArray = [
-    {
-      name: "Puppet",
-      photo: "http://www.leaderpost.com/mobile/news/latest-news/cms/binary/5762351.jpg?size=mobile290",
-      scores: [
+let friendArray = [{
+    name: "grouch",
+    photo: "http://www.leaderpost.com/mobile/news/latest-news/cms/binary/5762351.jpg?size=mobile290",
+    scores: [
+        1,
+        1,
+        5,
+        3,
+        1,
+        1,
+        3,
+        3,
+        1,
+        5
+    ]
+    },{
+    name: "Elmo",
+    photo: "https://www.depends-on-the-definition.com/wp-content/uploads/2018/07/elmo_400x400.jpg",
+    scores: [
         5,
         1,
         4,
@@ -13,12 +27,67 @@ var friendArray = [
         5,
         4,
         1
-      ]
-    }
-  ];
-  
+    ]
+    },{
+    name: "Big Bird",
+    photo: "https://i.ytimg.com/vi/3OYuJKvr9f4/maxresdefault.jpg",
+    scores: [
+        3,
+        1,
+        2,
+        2,
+        5,
+        3,
+        2,
+        4,
+        1,
+        4
+    ]
+    },{
+    name: "Cookie Monster",
+    photo: "https://fishersindianaestateplanning.typepad.com/.a/6a0192ac0685fd970d01bb09008496970d-320wi",
+    scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+]
+    },{
+    name: "The Count",
+    photo: "https://horrorpediadotcom.files.wordpress.com/2013/05/1778314-png.jpeg",
+    scores: [
+        1,
+        2,
+        3,
+        1,
+        2,
+        3,
+        1,
+        2,
+        3,
+        1
+]
+    },{
+    name: "Mr. Snuffleupagus",
+    photo: "https://i.pinimg.com/originals/7b/08/e1/7b08e19a9226a642dbd266a8ac0abe50.jpg",
+    scores: [
+        3,
+        5,
+        1,
+        4,
+        5,
+        1,
+        5,
+        5,
+        3,
+        1
+]
+}];
+
 module.exports = friendArray;
-
-
-
-  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,49 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    $("#submit").on("click", function(event) {
-        event.preventDefault();
-        if (validateForm()) {
-            friendArray.push(req.body);
-            res.json(true);
-        }
+    let macthDiff = 0;
+    let bestMatch = {
+        name: "",
+        photo: "",
+        friendDiff: 1000
+    };
+    let userData = req.body;
+    let userName = userData.name;
+    let userScores = userData.scores
+
+    let b = userScores.map(function(item){
+        return parseInt(item, 10);
     })
+
+    userData = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: b
+    }
+
+    let sum = b.reduce((a,b) => a + b, 0);
+
+    for (let i = 0; i < friends.length; i++) {
+        macthDiff = 0;
+        let newFriendScore = friends[i].scores.reduce((a,b) => a + b, 0);
+        macthDiff += Math.abs(sum - newFriendScore);
+
+        if(macthDiff <= bestMatch.friendDiff){
+            bestMatch.name = friends[i].name
+            bestMatch.photo = friends[i].photo
+            bestMatch.friendDiff = friends[i].macthDiff;
+        }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
   });
+
+
+
+//   app.post("/api/friends", function(req, res) {
+//     // Empty out the arrays of data
+//     friends.length = [];
+//     res.json({ ok: true });
+//   });
 };
 
